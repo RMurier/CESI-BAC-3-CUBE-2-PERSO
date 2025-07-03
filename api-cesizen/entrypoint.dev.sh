e7390aeeccdd36574daf6bcb3fed4e9a359d2ea0
RMurier <murierromain@gmail.com>
2025-07-03 21:09:23 +0200

    fix https

---
+++ api-cesizen/entrypoint.dev.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "📦 Génération Prisma client..."
+npx prisma generate
+
+echo "🌱 Exécution des migrations..."
+npx prisma db push
+
+echo "🌱 Lancement du seed..."
+npx prisma db seed
+
+echo "🏗️ Build de l'application Next.js..."
+npm run build
+
+echo "🚀 Lancement de l'API..."
+node server-https.js -H 0.0.0.0
